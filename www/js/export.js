7fe2ab8c5d21f43904ea0f34d35749484dd7fe7f
Catharina Hahnfeld <c.h4hnfeld@gmail.com>
2015-01-26 13:50:31 +0100

    fixed html export and delete FileSaver.js

--- www/js/export.js
+++ www/js/export.js
@@ -1,12 +1,27 @@
-var text = $("#html-output")[0];
-var html_begin = "<html><head><meta charset='utf-8'></head><body>"
-var html_end = "</body></html>"
-xml_serializer = new XMLSerializer
-html_output = html_begin + xml_serializer.serializeToString(text) + html_end;
+var saveData = (function () {
+    var a = document.createElement("a");
+    document.body.appendChild(a);
+    a.style = "display: none";
+    var data_type = "plain";
+    return function (data, fileName, type) {
+    		if(type == "html"){ data_type = "html"};
+            blob = new Blob([data], {type: "text/"+ data_type +";charset="+ document.characterSet}),
+            url = window.URL.createObjectURL(blob);
+        a.href = url;
+        a.download = fileName;
+        a.click();
+        window.URL.revokeObjectURL(url);
+    };
+}());
+
 
 $('#btn-export-html').click( function(){
-  	var blob = new Blob([html_output], {type: "text/html;charset="+ document.characterSet});
-  	saveAs(blob, "export.html");
+	var text = $("#html-output")[0];
+	var html_begin = "<html><head><meta charset='utf-8'></head><body>"
+	var html_end = "</body></html>"
+	xml_serializer = new XMLSerializer
+	html_output = html_begin + xml_serializer.serializeToString(text) + html_end;
+	saveData(html_output, "export.html", "html");
 });
 
 $('#btn-export-pdf').click( function(){
@@ -25,6 +40,5 @@ $('#btn-export-pdf').click( function(){
 
 $('#btn-export-md').click( function(){
 	var text = $("#md-input").val();
-  	var blob = new Blob([text], {type: "text/plain;charset=utf-8"});
-  	saveAs(blob, "export.md");
+  	saveData(text, "export.md", "md")
 });
